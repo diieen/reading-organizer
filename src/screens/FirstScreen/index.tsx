ea1b7e84a7711f965934dd090c4f255516c1e25e
Jean Ferreira <jean@prumo.digital>
2022-04-01 15:16:47 -0300

    added search file

--- src/screens/FirstScreen/index.tsx
+++ src/screens/FirstScreen/index.tsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { Text, SafeAreaView, StatusBar, Button, Linking } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import * as DocumentPicker from 'expo-document-picker';
 
 import { styles } from './style'
 
+interface props {
+    name: string;
+    uri: string
+}
+
 export function FirstScreen() {
-    const [text, setText] = useState('');
+    const [fileResponse, setFileResponse] = useState<props>({name: "", uri: ""});
+
+    const handleDocumentSelection = useCallback(async () => {
+        try {
+            const response : any = await DocumentPicker.getDocumentAsync({
+                type: 'application/pdf',
+            });
+            setFileResponse(response);
+        } catch (err) {
+            console.warn(err);
+        }
+    }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Hello World!</Text>
-
-            <TextInput
-                style={styles.input}
-                onChangeText={ setText }
-            />
-
-            <Text>
-                Você digitou: { text }
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle={'dark-content'} />
+            <Text onPress={() => Linking.openURL(fileResponse.uri)}>
+                {fileResponse.name}
             </Text>
-        </View>
+
+            <Button title="Select 📑" onPress={handleDocumentSelection} />
+        </SafeAreaView>
     );
 }
